c1c24536be54855cb3e18d72383e6ee5cfe149b0
Francis Aleke <ALEKEFRANCIS0@gmail.com>
2020-11-11 01:50:40 -0500

    created card.js and styled it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
 import LineGraph from './components/LineGraph';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <div>
+          <h1> World wide corona report</h1>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center'
+          }}>
+            <Card>
+              <span>Total Confirmed</span><br />
+              <span>0</span>
+            </Card>
+            <Card>
+              <span>Total Recovered</span><br />
+              <span>0</span>
+            </Card>
+            <Card>
+              <span>Total Deaths</span><br />
+              <span>0</span>
+            </Card>
+          </div>
+          
+      </div>
+    </div>
     <LineGraph/>
       
     </div>
