0b2d88aa96d7487c50a5cf66fac526a34ec5ef1d
Francis Aleke <ALEKEFRANCIS0@gmail.com>
2020-11-11 01:24:34 -0500

    Added a responsive graph and also resized it

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Line } from 'react-chartjs-2';
 
 
 /** 
@@ -8,7 +9,40 @@ import React from 'react'
 
  const LineGraph = (props) => {
      return(
-         <div>LineGraph</div>
+    <div
+        style={{
+            width:'600px',
+            height:'600px',
+            margin:'50px auto'
+        }}
+    >
+            <Line data = {{
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }
+        ]
+    }} />
+</div>
      )
  }
 
