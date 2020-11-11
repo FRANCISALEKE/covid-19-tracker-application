8594af56b7cfa73174c6350581618e3c316aa1fd
Francis Aleke <ALEKEFRANCIS0@gmail.com>
2020-11-11 03:31:10 -0500

    added the countries and a select drop down

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import LineGraph from './components/LineGraph';
 import CovidSummary from './components/CovidSummary';
@@ -6,22 +6,62 @@ import axios from './axios';
 
 function App() {
 
+    const [totalConfirmed, setTotalConfirmed] = useState(0);
+    const [totalRecovered, setTotalRecovered] = useState(0);
+    const [totalDeaths, setTotalDeaths] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [covidSummary, setCovidSummary] = useState ({})
 
   useEffect(() => {
     
 
+    setLoading(true);
+    axios.get('/summary')
+    .then(res => {
+      setLoading(false)
 
-  }, [])
+
+      if(res.status === 200){
+        setTotalConfirmed(res.data.Global.TotalConfirmed);
+        setTotalConfirmed(res.data.Global.NewRecovered);
+        setTotalConfirmed(res.data.Global.TotalDeaths); 
+        setCovidSummary(res.data);
+
+      }
+
+      
+      console.log(res);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+      
+
+  }, []);
+
+
+  if(loading){
+    return <p>Fetching data from api...! </p>
+  }
 
   return (
     <div className="App">
       <CovidSummary 
-        totalConfirmed={0}
-        totalRecovered={0}
-        totalDeaths={0}
-        country={'Nigeria'}
-      />
-    <LineGraph/>
+          totalConfirmed={totalConfirmed}
+          totalRecovered={totalRecovered}
+          totalDeaths={totalDeaths}
+          country={''}
+     />
+
+     <div>
+      <select>
+        {
+          covidSummary.Countries && covidSummary.Countries.map(country => <option value={country.Slug}>{country.Country}</option>)
+        }
+      </select>
+     
+     </div>
+        <LineGraph/>
       
     </div>
   );
