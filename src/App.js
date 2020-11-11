cadd2d775527ccb5e23de4616c5f43178e2983a3
Francis Aleke <ALEKEFRANCIS0@gmail.com>
2020-11-11 01:04:29 -0500

    created components folder and linegraph.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LineGraph from './components/LineGraph';
+
+function App() {
+  return (
+    <div className="App">
+    <LineGraph/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LineGraph.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+/** 
+ * @author
+ * @function LineGraph
+ **/
+
+ const LineGraph = (props) => {
+     return(
+         <div>LineGraph</div>
+     )
+ }
+
+export default LineGraph;
+
